d10d1814ac6344f434209138bb56bc3a5ba2a9c7
liolok <liolokit@gmail.com>
2019-03-09 00:01:30 +0800

    feat: add main activity with bottom navigation

---
+++ app/src/main/java/top/gpg2/messages/MainActivity.kt
@@ -0,0 +1,12 @@
+package top.gpg2.messages
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
