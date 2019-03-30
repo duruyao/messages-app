392d5b8e805206079c1b93296e2c6913e8510d0e
graycat0918 <graycat0918@gmail.com>
2019-03-30 13:14:50 +0800

    feat: add `SessionsAdapter.java` & `SessionsDisplayer.java`.

---
+++ app/src/main/java/com/dry/messages/SessionsAdapter.java
@@ -0,0 +1,126 @@
+package com.dry.messages;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.media.MediaCas;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import top.gpg2.messages.R;
+
+import java.util.List;
+
+/**
+ * An adapter that connects RecyclerView and List of sessions(that contain of all the messages about one phone number).
+ *
+ * @author DuRuyao
+ * Create 19/03/30
+ */
+public class SessionsAdapter extends RecyclerView.Adapter<SessionsAdapter.ViewHolder> {
+    private List<List<Messages>> sessionsList;
+    private Context context;
+
+    /**
+     * An internal class to cache instance of controls.
+     */
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        View sessionsView;
+        TextView messagesAddress;
+        TextView messagesBody;
+
+        /**
+         * Constructor of ViewHolder class.
+         *
+         * @param view The outermost layout of item of RecyclerView.
+         */
+        private ViewHolder(View view) {
+            super(view);
+            sessionsView = view;
+            messagesAddress = (TextView) view.findViewById(R.id.messages_address);
+            messagesBody = (TextView) view.findViewById(R.id.messages_body);
+        }
+    }
+
+    /**
+     * Constructor of MessagesAdapter class to import data list.
+     *
+     * @param context      Current context.
+     * @param sessionsList List of sessions(that contain of all the messages about one phone number).
+     */
+    public SessionsAdapter(Context context, List<List<Messages>> sessionsList) {
+        this.sessionsList = sessionsList;
+        this.context = context;
+    }
+
+    /**
+     * Get a ViewHolder that contains of the item of RecyclerLayout.
+     *
+     * @return A instance of ViewHolder.
+     */
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        /* Load the item of RecyclerLayout. */
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.messages_item,
+                parent, false);
+        /* Create instance of ViewHolder by passing the view parameter. */
+        final ViewHolder holder = new ViewHolder(view);
+
+        holder.sessionsView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent();
+                Log.d("110", "Create a Intent.");
+                int position = holder.getAdapterPosition();
+                Messages messages = sessionsList.get(position).get(0);
+
+
+            }
+        });
+        /* Return the instance of ViewHolder. */
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Messages messages = sessionsList.get(position).get(0);
+        holder.messagesAddress.setText(messages.getAddress());
+        holder.messagesBody.setText(messages.getBody());
+    }
+
+    /**
+     * Get number of item of RecyclerView.
+     *
+     * @return A number.
+     */
+    @Override
+    public int getItemCount() {
+        return sessionsList.size();
+    }
+
+    /**
+     * Get current activity.
+     *
+     * @param context Current context.
+     * @return An activity.
+     */
+    private Activity getActivity(Context context) {
+        while (!(context instanceof Activity) && context instanceof ContextWrapper) {
+            context = ((ContextWrapper) context).getBaseContext();
+        }
+
+        if (context instanceof Activity) {
+            return (Activity) context;
+        } else {
+            return null;
+        }
+    }
+}
--- app/src/main/java/com/dry/messages/SessionsDisplayer.java
+++ app/src/main/java/com/dry/messages/SessionsDisplayer.java
@@ -7,34 +7,34 @@ import android.content.ContextWrapper;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.provider.ContactsContract;
 import android.util.Log;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import top.gpg2.messages.R;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
- * A class that could be called to display messages.
+ * A class that could be called to display sessions.
  *
  * @author DuRuyao
- * Create 19/03/28
+ * Create 19/03/30
  */
-public class MessagesDisplayer {
+public class SessionsDisplayer {
+
     final private int READ_SMS_REQUEST_CODE = 121;
     final String SMS_URI_ALL = "content://sms/";
-
-    private List<Messages> messagesList = new ArrayList<>();
+    private List<Messages> allMessagesList = new ArrayList<>();
+    private List<List<Messages>> sessionsList = new ArrayList<List<Messages>>();
     private Context context;
     private Activity activity;
 
-    public MessagesDisplayer(Context context) {
+    public SessionsDisplayer(Context context) {
         this.context = context;
         this.activity = getActivity(this.context);
 
@@ -44,17 +44,41 @@ public class MessagesDisplayer {
             Log.d("110", "No permission, and I'll request it.");
         } else {
             Log.d("110", "Have permission, and I'll read SMS.");
-            readSMS();
+            getSessions();
         }
 
         RecyclerView recyclerView = (RecyclerView) activity.findViewById(R.id.recyclerView1);
         LinearLayoutManager layoutManager = new LinearLayoutManager(context);
         recyclerView.setLayoutManager(layoutManager);
-        MessagesAdapter adapter = new MessagesAdapter(this.context, messagesList);
+        SessionsAdapter adapter = new SessionsAdapter(this.context, sessionsList);
         recyclerView.setAdapter(adapter);
     }
 
-    public void readSMS() {
+    public void getSessions() {
+        allMessagesList = getAllSMSList();
+        Iterator<Messages> mIterator = allMessagesList.iterator();
+        while (mIterator.hasNext()) {
+            List<Messages> mList = new ArrayList<>();
+
+            Messages messages1 = (Messages) mIterator.next();
+            mList.add(messages1);
+            String ADDRESS = messages1.getAddress();
+            allMessagesList.remove(mIterator.next());
+            while (mIterator.hasNext()) {
+                Messages messages2 = (Messages) mIterator.next();
+                if (messages2.getAddress() == ADDRESS) {
+                    mList.add(messages2);
+                    allMessagesList.remove(mIterator.next());
+                }
+            }
+            sessionsList.add(mList);
+        }
+
+
+    }
+
+    public List<Messages> getAllSMSList() {
+        List<Messages> allMessagesList = new ArrayList<>();
         Log.d("110", "Call function readSMS().");
         Cursor cursor = null;
         try {
@@ -66,7 +90,7 @@ public class MessagesDisplayer {
                     String messagesBody =
                             cursor.getString(cursor.getColumnIndex("body"));
 
-                    messagesList.add(new Messages(messagesAddress, messagesBody));
+                    allMessagesList.add(new Messages(messagesAddress, messagesBody));
                 }
             }
         } catch (Exception e) {
@@ -76,6 +100,7 @@ public class MessagesDisplayer {
                 cursor.close();
             }
         }
+        return allMessagesList;
     }
 
     private Activity getActivity(Context context) {
@@ -89,5 +114,4 @@ public class MessagesDisplayer {
             return null;
         }
     }
-
 }
